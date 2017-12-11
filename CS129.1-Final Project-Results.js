4e6d45b07df5af97dffa6a764a337bf69aecf22f
araosgabrielle <34431646+araosgabrielle@users.noreply.github.com>
2017-12-11 12:06:32 +0800

    Add files via upload

---
+++ CS129.1-Final Project-Results.js
@@ -0,0 +1,237 @@
+//Total mrt traffic per hour in descending order
+
+{ "_id" : "18:00 0 18:59", "traffic" : 26895751 }
+{ "_id" : "17:00 0 17:59", "traffic" : 25742529 }
+{ "_id" : "08:00 0 08:59", "traffic" : 25339920 }
+{ "_id" : "19:00 0 19:59", "traffic" : 25079609 }
+{ "_id" : "07:00 0 07:59", "traffic" : 24716227 }
+{ "_id" : "09:00 0 09:59", "traffic" : 22867655 }
+{ "_id" : "20:00 0 20:59", "traffic" : 21232439 }
+{ "_id" : "16:00 0 16:59", "traffic" : 21011682 }
+{ "_id" : "10:00 0 10:59", "traffic" : 18980874 }
+{ "_id" : "15:00 0 15:59", "traffic" : 18390042 }
+{ "_id" : "11:00 0 11:59", "traffic" : 18362848 }
+{ "_id" : "06:00 0 06:59", "traffic" : 18279567 }
+{ "_id" : "12:00 0 12:59", "traffic" : 18202207 }
+{ "_id" : "13:00 0 13:59", "traffic" : 17825944 }
+{ "_id" : "14:00 0 14:59", "traffic" : 17637795 }
+{ "_id" : "21:00 0 21:59", "traffic" : 15948586 }
+{ "_id" : "05:00 0 05:59", "traffic" : 7457522 }
+{ "_id" : "22:00 0 22:59", "traffic" : 5839272 }
+{ "_id" : "04:00 0 04:59", "traffic" : 189189 }
+{ "_id" : "23:00 0 23:59", "traffic" : 87805 }
+{ "_id" : "03:00 0 03:59", "traffic" : 721 }
+{ "_id" : "00:00 0 00:59", "traffic" : 27 }
+{ "_id" : "01:00 0 01:59", "traffic" : 9 }
+{ "_id" : "02:00 0 02:59", "traffic" : 6 }
+
+
+
+
+// Traffic per month
+{ "_id" : "July", "traffic" : 32228188 }
+{ "_id" : "October", "traffic" : 30479158 }
+{ "_id" : "January", "traffic" : 30350911 }
+{ "_id" : "May", "traffic" : 29275617 }
+{ "_id" : "September", "traffic" : 29261592 }
+{ "_id" : "November", "traffic" : 29008807 }
+{ "_id" : "April", "traffic" : 28887043 }
+{ "_id" : "August", "traffic" : 28855323 }
+{ "_id" : "June", "traffic" : 28806572 }
+{ "_id" : "December", "traffic" : 28712706 }
+{ "_id" : "Febuary", "traffic" : 28073119 }
+{ "_id" : "March", "traffic" : 26149190 }
+
+// Traffic per station per month
+{
+        "_id" : "December",
+        "natraffic" : 3908093,
+        "qatraffic" : 2175006,
+        "gktraffic" : 1062274,
+        "cutraffic" : 3281977,
+        "sttraffic" : 606990,
+        "ortraffic" : 1521364,
+        "sbtraffic" : 3031021,
+        "batraffic" : 1276059,
+        "gutraffic" : 1962307,
+        "butraffic" : 966294,
+        "aytraffic" : 2565140,
+        "mgtraffic" : 1694559,
+        "tftraffic" : 4661622
+}
+{
+        "_id" : "November",
+        "natraffic" : 3835687,
+        "qatraffic" : 2247648,
+        "gktraffic" : 1061419,
+        "cutraffic" : 3135768,
+        "sttraffic" : 626908,
+        "ortraffic" : 1550341,
+        "sbtraffic" : 3097454,
+        "batraffic" : 1400100,
+        "gutraffic" : 1982514,
+        "butraffic" : 1042535,
+        "aytraffic" : 2694037,
+        "mgtraffic" : 1785308,
+        "tftraffic" : 4549088
+}
+{
+        "_id" : "April",
+        "natraffic" : 3982775,
+        "qatraffic" : 2107217,
+        "gktraffic" : 1050239,
+        "cutraffic" : 3293106,
+        "sttraffic" : 578894,
+        "ortraffic" : 1441060,
+        "sbtraffic" : 2957189,
+        "batraffic" : 1338038,
+        "gutraffic" : 2041467,
+        "butraffic" : 1089875,
+        "aytraffic" : 2589097,
+        "mgtraffic" : 1747029,
+        "tftraffic" : 4671057
+}
+{
+        "_id" : "January",
+        "natraffic" : 4147198,
+        "qatraffic" : 2239364,
+        "gktraffic" : 1149195,
+        "cutraffic" : 3435327,
+        "sttraffic" : 642275,
+        "ortraffic" : 1537262,
+        "sbtraffic" : 3119286,
+        "batraffic" : 1443498,
+        "gutraffic" : 2157794,
+        "butraffic" : 1102956,
+        "aytraffic" : 2714451,
+        "mgtraffic" : 1830352,
+        "tftraffic" : 4831953
+}
+{
+        "_id" : "September",
+        "natraffic" : 4003721,
+        "qatraffic" : 2226495,
+        "gktraffic" : 1073388,
+        "cutraffic" : 3225365,
+        "sttraffic" : 625824,
+        "ortraffic" : 1516535,
+        "sbtraffic" : 3110520,
+        "batraffic" : 1422761,
+        "gutraffic" : 2124668,
+        "butraffic" : 1123594,
+        "aytraffic" : 2542819,
+        "mgtraffic" : 1788721,
+        "tftraffic" : 4477181
+}
+{
+        "_id" : "July",
+        "natraffic" : 4329361,
+        "qatraffic" : 2356654,
+        "gktraffic" : 1133215,
+        "cutraffic" : 3611587,
+        "sttraffic" : 646462,
+        "ortraffic" : 1593117,
+        "sbtraffic" : 3300528,
+        "batraffic" : 1560515,
+        "gutraffic" : 2335215,
+        "butraffic" : 1208245,
+        "aytraffic" : 3058591,
+        "mgtraffic" : 1989017,
+        "tftraffic" : 5105681
+}
+{
+        "_id" : "May",
+        "natraffic" : 4088387,
+        "qatraffic" : 2154173,
+        "gktraffic" : 1040619,
+        "cutraffic" : 3385644,
+        "sttraffic" : 582270,
+        "ortraffic" : 1459135,
+        "sbtraffic" : 3044142,
+        "batraffic" : 1356815,
+        "gutraffic" : 2077121,
+        "butraffic" : 1056144,
+        "aytraffic" : 2526720,
+        "mgtraffic" : 1750546,
+        "tftraffic" : 4753901
+}
+{
+        "_id" : "June",
+        "natraffic" : 3942447,
+        "qatraffic" : 2117316,
+        "gktraffic" : 1015570,
+        "cutraffic" : 3296388,
+        "sttraffic" : 568693,
+        "ortraffic" : 1427884,
+        "sbtraffic" : 2979724,
+        "batraffic" : 1383794,
+        "gutraffic" : 2092118,
+        "butraffic" : 1041987,
+        "aytraffic" : 2525798,
+        "mgtraffic" : 1802647,
+        "tftraffic" : 4612206
+}
+{
+        "_id" : "March",
+        "natraffic" : 3583168,
+        "qatraffic" : 1929187,
+        "gktraffic" : 972904,
+        "cutraffic" : 2951919,
+        "sttraffic" : 535856,
+        "ortraffic" : 1313139,
+        "sbtraffic" : 2720594,
+        "batraffic" : 1219297,
+        "gutraffic" : 1828122,
+        "butraffic" : 937708,
+        "aytraffic" : 2324801,
+        "mgtraffic" : 1606220,
+        "tftraffic" : 4226275
+}
+{
+        "_id" : "Febuary",
+        "natraffic" : 3787225,
+        "qatraffic" : 2087531,
+        "gktraffic" : 1064459,
+        "cutraffic" : 3167648,
+        "sttraffic" : 579206,
+        "ortraffic" : 1411057,
+        "sbtraffic" : 2876932,
+        "batraffic" : 1326573,
+        "gutraffic" : 2007029,
+        "butraffic" : 1026257,
+        "aytraffic" : 2445611,
+        "mgtraffic" : 1764093,
+        "tftraffic" : 4529498
+}
+{
+        "_id" : "August",
+        "natraffic" : 3949691,
+        "qatraffic" : 2113625,
+        "gktraffic" : 1011775,
+        "cutraffic" : 3148147,
+        "sttraffic" : 564132,
+        "ortraffic" : 1461819,
+        "sbtraffic" : 2992262,
+        "batraffic" : 1366684,
+        "gutraffic" : 2115999,
+        "butraffic" : 1057208,
+        "aytraffic" : 2814488,
+        "mgtraffic" : 1716766,
+        "tftraffic" : 4542727
+}
+{
+        "_id" : "October",
+        "natraffic" : 4122301,
+        "qatraffic" : 2293236,
+        "gktraffic" : 1128106,
+        "cutraffic" : 3335491,
+        "sttraffic" : 674753,
+        "ortraffic" : 1641282,
+        "sbtraffic" : 3218198,
+        "batraffic" : 1358077,
+        "gutraffic" : 2082247,
+        "butraffic" : 1176463,
+        "aytraffic" : 2909182,
+        "mgtraffic" : 1885085,
+        "tftraffic" : 4654737
+}
